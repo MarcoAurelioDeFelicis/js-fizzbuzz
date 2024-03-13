8d2dfeffecba39d3b8ab3167bf1cb6e3afed44a6
MarcoAurelioDeFelicis <marcodefelicis24@gmail.com>
2024-03-13 21:12:15 +0100

    ADD: pulizia codicde senza sytampa di numeri (verifica) insieme a (Buzz,Fizz,FizzBuzz)

--- js/app.js
+++ js/app.js
@@ -15,17 +15,17 @@ for ( let i = 0; i < 100; i++) {
     // se num è divisibile per 3 e 3 c.l di FizzBuzz
     if (resto3e5 === 0) {
 
-        console.log (num + 'FizzBuzz')
+        console.log ('FizzBuzz')
     }
     // se num è divisibile per 3 c.l di Fizz
     else if (resto3 === 0) {
 
-        console. log (num + 'Fizz')
+        console. log ('Fizz')
     }
     // se num è divisibile per 5 c.l di Buzz 
     else if (resto5 === 0) {
 
-        console.log (num + 'Buzz')
+        console.log ('Buzz')
     }
 
     else {
